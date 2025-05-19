bcd08d8e538ea184fa0d71b71bf7e7b7e24e4e9f
Nicolas Henrique Alves dos Santos <164679510+Nicoo200@users.noreply.github.com>
2025-05-19 14:19:21 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,36 @@
-
 import React, { useState, useEffect } from 'react';
 import { View, ActivityIndicator } from 'react-native';
-import Navigation from './src/Navigation';
-import loadFonts from './src/LoadFonts';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
-  // const [fontsLoaded] = useFonts({
-  //   'Goldman-Bold': require('./assets/fonts/Goldman-Bold.ttf'),
-  //   'Goldman-Regular': require('./assets/fonts/Goldman-Regular.ttf'),
-  //   'Geologica-SemiBold': require('./assets/fonts/Geologica-SemiBold.ttf'),
-  // });
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  // if (!fontsLoaded) {
-  //   return (
-  //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-  //       <ActivityIndicator size="large" />
-  //     </View>
-  //   );
-  // }
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await loadFonts();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setFontsLoaded(true);
+      }
+    }
 
-  return <Navigation />;
+    prepare();
+  }, []);
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#3D1047" />
+      </View>
+    );
+  }
+
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
